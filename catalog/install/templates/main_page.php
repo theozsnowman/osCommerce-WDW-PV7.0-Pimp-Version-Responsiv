b54b306d47755429313ef8d2e60e1b9fdb58ae02
Manfred Wedel <mcmannehan@users.noreply.github.com>
2017-02-01 12:05:59 +0700

    Master Update 31. Jan. 2017

--- catalog/install/templates/main_page.php
+++ catalog/install/templates/main_page.php
@@ -49,7 +49,7 @@
   <div class="clearfix"></div>
 
   <?php require('templates/pages/' . $page_contents); ?>
-https://github.com/mcmannehan/osCommerce-WDW-PV7.0-Pimp-Version-Responsiv
+
   <footer>
     <div class="text-center well well-sm"><p><a href="https://github.com/mcmannehan/osCommerce-WDW-PV7.0-Pimp-Version-Responsiv" target="_blank">osCommerce WDW-PV7.0 Pimp Version Responsiv</a> Based on osCommerce Online Merchant Copyright &copy; 2000-<?php echo date('Y'); ?> <a href="http://www.oscommerce.com" target="_blank">osCommerce</a> (<a href="http://www.oscommerce.com/Us&amp;Legal" target="_blank">Copyright and Trademark Policy</a>)</p></div>
   </footer>
