a0657244d9cfddebf11c090036380e39eade1e03
mcmannehan <webmaster@webdesign-wedel.de>
2017-02-15 17:05:59 +0700

    del

--- catalog/ext/api/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Gyakutsuki
- * @copyright Portions Copyright 2015 osCommerce
- * @license GNU Public License GPL
- * @version $Id:
- */
-   header("Location: http://".$_SERVER['HTTP_HOST']."/");
-?>
